09f516398221f0c31d458988f12e25570be8af99
Ruslan Malogulko <ruslan.malogulko@gmail.com>
2019-04-28 15:39:50 +0300

    Revert "add hw 6"

--- hw6/hw6/main6.1.js
+++
@@ -1,8 +0,0 @@
-console.log(document.head);
-console.log(document.body);
-console.log(document.body.children);
-console.log(document.querySelector('div').children);
-console.log(Array.prototype.slice.call( document.body.children, 1, -1));
-
-
-
