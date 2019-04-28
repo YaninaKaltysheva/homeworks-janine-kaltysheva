34e5a070310205f21328b07d1954eddacc5bae06
JanineKaltysheva <46133931+JanineKaltysheva@users.noreply.github.com>
2019-04-28 16:07:49 +0300

    изменила isParent

--- hw6/hw6/main6.js
+++ hw6/hw6/main6.js
@@ -9,43 +9,23 @@
 
 function isParent (parent, child){
     if (child.parentElement === parent){
-        return true
-    } else {
-        return false
-    }
-return isParent
+        return child.parentElement === parent;}
 };
 console.log(isParent(document.body.children[0], document.querySelector('mark')));
-
 console.log(isParent(document.querySelector('ul'), document.querySelector('mark')));
-
 //2. Получить список всех ссылок, которые не находятся внутри списка ul
-
 const linkNotFromUl =  Array.from(document.querySelectorAll('body a')).filter(link => !link.closest('ul'));
-
 console.log(linkNotFromUl)
-
 // 3. Найти элемент, который находится перед и после списка ul
-
 const newElement = () => {
     console.log(document.querySelector('ul').previousElementSibling);
     console.log(document.querySelector('ul').nextElementSibling);
     return newElement;
-
 }
-
 newElement()
-
-
-
 //4. Посчитать количество элементов li в списке
-
 const countElement = () => {
     console.log(document.querySelectorAll('ul li').length);
     return countElement;
-
 }
-
 countElement()
-
-
