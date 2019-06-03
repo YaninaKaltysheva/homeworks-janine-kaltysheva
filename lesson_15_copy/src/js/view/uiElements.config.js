5d12dcb8ccc81734019a09d6cabf1779afac779a
JanineKaltysheva <46133931+JanineKaltysheva@users.noreply.github.com>
2019-06-03 19:07:14 +0300

    fixed export

--- lesson_15_copy/src/js/view/uiElements.config.js
+++ lesson_15_copy/src/js/view/uiElements.config.js
@@ -1,8 +1,5 @@
-    
-const form = document.forms['newsControlForm'];
-const countrySelect = document.forms['newsControlForm']['country'];
-const categorySelect = document.forms['newsControlForm']['category'];
-const newsContainer = document.querySelector('.news-wrap .row');
-const searchInput = document.forms['newsControlForm']['search'];
-
-export{form, countrySelect, categorySelect, newsContainer, searchInput}
+export const form = document.forms['newsControlForm'];
+export const countrySelect = document.forms['newsControlForm']['country'];
+export const categorySelect = document.forms['newsControlForm']['category'];
+export const newsContainer = document.querySelector('.news-wrap .row');
+export const searchInput = document.forms['newsControlForm']['search'];
